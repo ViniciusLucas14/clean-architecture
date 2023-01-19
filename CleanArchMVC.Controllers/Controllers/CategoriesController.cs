79acc8e0ba67aadf91a7f68decbe7bf26f3080a4
Vinicius Lucas <vlucassouza@gmail.com>
2023-01-18 21:58:17 -0300

    controller

--- CleanArchMVC.Controllers/Controllers/CategoriesController.cs
+++ CleanArchMVC.Controllers/Controllers/CategoriesController.cs
@@ -1,10 +1,13 @@
 using System;
+using CleanArchMVC.Application.DTOs;
 using CleanArchMVC.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMVC.Controllers.Controllers
 {
-    public class CategoriesController : Controller
+    [Route("v1/api/category")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _service;
         public CategoriesController(ICategoryService service)
@@ -12,9 +15,21 @@ namespace CleanArchMVC.Controllers.Controllers
             _service = service;
         }
         [HttpGet]
-        public async Task<IActionResult> GetCategoriesAsync()
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategoriesAsync()
         {
             return Ok(await _service.GetCategoriesAsync());
+        }
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            await _service.CreateAsync(categoryDTO);
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDTO.Id }, categoryDTO);
         }
     }
 }
